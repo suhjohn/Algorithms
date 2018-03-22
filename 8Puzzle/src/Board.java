3dd2e363811627132f96872707d317a286e8f07d
John Sangwon Suh <johnsuh94@gmail.com>
2018-03-23 00:56:51 +0900

    add solutions to 8puzzle

--- 8Puzzle/src/Board.java
+++ 8Puzzle/src/Board.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.ArrayList;
@@ -28,11 +29,10 @@ import java.util.Iterator;
  */
 
 public class Board {
-    private int[][] board;
-    private int[][] goalBoard;
+    private final int[][] board;
     private int blankI;
     private int blankJ;
-    private int n;
+    private final int n;
 
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
@@ -40,19 +40,20 @@ public class Board {
         this.board = blocks;
         this.n = blocks[0].length;
 
-        this.goalBoard = new int[this.n][this.n];
         for (int i = 0; i < this.n; i++) {
             for (int j = 0; j < this.n; j++) {
                 if (this.board[i][j] == 0) {
                     this.blankI = i;
                     this.blankJ = j;
                 }
-                this.goalBoard[i][j] = (i * this.n) + j + 1;
             }
         }
-        this.goalBoard[this.n - 1][this.n - 1] = 0;
     }
 
+    private int convert(int i, int j) {
+        if (i == this.n - 1 && j == this.n - 1) return 0;
+        return (i * this.n) + j + 1;
+    }
 
     // board dimension n
     public int dimension() {
@@ -68,8 +69,7 @@ public class Board {
         int count = 0;
         for (int i = 0; i < this.n; i++) {
             for (int j = 0; j < this.n; j++) {
-                if (this.goalBoard[i][j] != this.board[i][j]
-                        && this.board[i][j] != 0) count++;
+                if (this.board[i][j] != 0 && this.convert(i, j) != this.board[i][j]) count++;
             }
         }
         return count;
@@ -98,15 +98,24 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        return Arrays.deepEquals(this.board, this.goalBoard);
+        boolean isGoal = true;
+
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                if (this.convert(i, j) != this.board[i][j]) {
+                    isGoal = false;
+                    break;
+                }
+            }
+            if (!isGoal) break;
+        }
+
+        return isGoal;
     }
 
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
-        int[][] twinBoardBlocks = new int[n][n];
-        for (int i = 0; i < this.n; i++) {
-            System.arraycopy(this.board[i], 0, twinBoardBlocks[i], 0, this.n);
-        }
+        int[][] twinBoardBlocks = this.copyBoard();
 
         int i = 0, j = 0, exchI = 0, exchJ = 0;
         if (twinBoardBlocks[0][0] != 0) {
@@ -127,7 +136,11 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
         Board compareBoard = (Board) y;
+        if (this.n != compareBoard.n) return false;
         boolean eq = true;
         for (int i = 0; i < this.n; i++) {
             for (int j = 0; j < this.n; j++) {
@@ -143,112 +156,50 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return new Neighbors(this);
+        Queue<Board> neighbors = new Queue<>();
+        // Up
+        if (this.blankJ - 1 >= 0) this.enqueueNeighbor(neighbors, "up");
+        // Right
+        if (this.blankI + 1 < this.n) this.enqueueNeighbor(neighbors, "right");
+        // Down
+        if (this.blankJ + 1 < this.n) this.enqueueNeighbor(neighbors, "down");
+        // Left
+        if (this.blankI - 1 >= 0) this.enqueueNeighbor(neighbors, "left");
+        return neighbors;
     }
 
-    private class Neighbors implements Iterable<Board> {
-        private Board currBoard;
-
-        private Neighbors(Board currBoard) {
-            this.currBoard = currBoard;
+    private void enqueueNeighbor(Queue<Board> neighbors, String direction) {
+        int[][] copy = this.copyBoard();
+        switch (direction) {
+            case "up":
+                this.exch(copy, this.blankI, this.blankJ, this.blankI, this.blankJ - 1);
+                break;
+            case "right":
+                this.exch(copy, this.blankI, this.blankJ, this.blankI + 1, this.blankJ);
+                break;
+            case "down":
+                this.exch(copy, this.blankI, this.blankJ, this.blankI, this.blankJ + 1);
+                break;
+            case "left":
+                this.exch(copy, this.blankI, this.blankJ, this.blankI - 1, this.blankJ);
+                break;
         }
-
-        public Iterator<Board> iterator() {
-            return new NeighborsIterator(this.currBoard);
-        }
-
-        private class NeighborsIterator implements Iterator<Board> {
-            private ArrayList<Board> boardList = new ArrayList<>();
-            private int i = 0;
-
-            private NeighborsIterator(Board board) {
-                if (isValidSquare("up", board.blankI, board.n)) {
-                    this.addNeighborBoard(board, "up");
-                }
-                if (isValidSquare("down", board.blankI, board.n)) {
-                    this.addNeighborBoard(board, "down");
-                }
-                if (isValidSquare("left", board.blankJ, board.n)) {
-                    this.addNeighborBoard(board, "left");
-                }
-                if (isValidSquare("right", board.blankJ, board.n)) {
-                    this.addNeighborBoard(board, "right");
-                }
-            }
-
-            private boolean isValidSquare(String direction, int val, int n) {
-                boolean validSquare = false;
-                switch (direction) {
-                    case "up":
-                        validSquare = val - 1 >= 0;
-                        break;
-                    case "down":
-                        validSquare = val + 1 < n;
-                        break;
-                    case "left":
-                        validSquare = val - 1 >= 0;
-                        break;
-                    case "right":
-                        validSquare = val + 1 < n;
-                        break;
-                }
-                return validSquare;
-            }
-
-            private void addNeighborBoard(Board board, String direction) {
-                int[][] neighborBoard = new int[board.n][board.n];
-                for (int i = 0; i < board.n; i++) {
-                    System.arraycopy(board.board[i], 0, neighborBoard[i], 0, board.n);
-                }
-                switch (direction) {
-                    case "up": {
-                        int toSwitch = board.board[board.blankI - 1][board.blankJ];
-                        neighborBoard[board.blankI - 1][board.blankJ] = 0;
-                        neighborBoard[board.blankI][board.blankJ] = toSwitch;
-                        break;
-                    }
-                    case "down": {
-                        int toSwitch = board.board[board.blankI + 1][board.blankJ];
-                        neighborBoard[board.blankI + 1][board.blankJ] = 0;
-                        neighborBoard[board.blankI][board.blankJ] = toSwitch;
-                        break;
-                    }
-                    case "left": {
-                        int toSwitch = board.board[board.blankI][board.blankJ - 1];
-                        neighborBoard[board.blankI][board.blankJ - 1] = 0;
-                        neighborBoard[board.blankI][board.blankJ] = toSwitch;
-                        break;
-                    }
-                    case "right": {
-                        int toSwitch = board.board[board.blankI][board.blankJ + 1];
-                        neighborBoard[board.blankI][board.blankJ + 1] = 0;
-                        neighborBoard[board.blankI][board.blankJ] = toSwitch;
-                        break;
-                    }
-                }
-                this.boardList.add(new Board(neighborBoard));
-            }
-
-            @Override
-            public boolean hasNext() {
-                return this.boardList.size() > this.i;
-            }
-
-            @Override
-            public void remove() {
-                this.boardList.remove(this.i);
-                this.i--;
-            }
-
-            @Override
-            public Board next() {
-                Board curr = this.boardList.get(this.i);
-                this.i++;
-                return curr;
-            }
-        }
+        neighbors.enqueue(new Board(copy));
     }
 
+    private int[][] copyBoard() {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < this.n; i++) {
+            System.arraycopy(this.board[i], 0, copy[i], 0, this.n);
+        }
+        return copy;
+    }
+
+    private void exch(int[][] board, int i1, int j1, int i2, int j2) {
+        int temp = board[i1][j1];
+        board[i1][j1] = board[i2][j2];
+        board[i2][j2] = temp;
+    }
 
     // string representation of this board (in the output format specified below)
     public String toString() {
@@ -285,12 +236,7 @@ public class Board {
         System.out.println("Initial twin board");
         System.out.println(initial.twin().toString());
 
-        System.out.println("Initial GoalBoard");
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(initial.goalBoard[i]));
-        }
-
-        for (Board neighbor:initial.neighbors()) {
+        for (Board neighbor : initial.neighbors()) {
             System.out.println("Neighbor");
             System.out.println(neighbor.toString());
         }
--- 8Puzzle/src/Solver.java
+++ 8Puzzle/src/Solver.java
@@ -1,26 +1,123 @@
-//public class Solver {
-//    // find a solution to the initial board (using the A* algorithm)
-//    public Solver(Board initial) {
-//
-//    }
-//
-//    // is the initial board solvable?
-//    public boolean isSolvable() {
-//
-//    }
-//
-//    // min number of moves to solve initial board; -1 if unsolvable
-//    public int moves() {
-//
-//    }
-//
-//    // sequence of boards in a shortest solution; null if unsolvable
-//    public Iterable<Board> solution() {
-//
-//    }
-//
-//    // solve a slider puzzle (given below)
-//    public static void main(String[] args) {
-//
-//    }
-//}
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Solver {
+    // find a solution to the initial board (using the A* algorithm)
+    private int totalMove = 0;
+    private SearchNode LastNode = null;
+    private boolean solvable = false;
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private final Board board;
+        private final SearchNode prevNode;
+        private final int move, manhattan, priority;
+
+        private SearchNode(Board board, SearchNode prevNode) {
+            this.board = board;
+            this.prevNode = prevNode;
+            this.move = prevNode != null ? prevNode.move + 1 : 0;
+            // Second opotimization
+            this.manhattan = board.manhattan();
+            this.priority = this.manhattan + this.move;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            int comp = Integer.compare(this.priority, that.priority);
+            if (comp == 0) comp = Integer.compare(this.manhattan, that.manhattan);
+//            if (comp == 0) comp = Integer.compare(this.board.hamming(), that.board.hamming());
+            return comp;
+        }
+    }
+
+    public Solver(Board initial) {
+        // Define a Search node and twin Search node.
+        SearchNode initialNode = new SearchNode(initial, null);
+        SearchNode initialNodeTwin = new SearchNode(initial.twin(), null);
+        // Insert the initial search node (the initial board, 0 moves, and a null predecessor search node) into Heap.
+        MinPQ<SearchNode> queue = new MinPQ<>();
+        MinPQ<SearchNode> twinQueue = new MinPQ<>();
+        queue.insert(initialNode);
+        twinQueue.insert(initialNodeTwin);
+
+        // Repeat the following procedure until the search node dequeued corresponds to a goal board:
+        //      Delete from the priority queue the search node with the minimum priority,
+        //      and insert onto the priority queue all neighboring search nodes
+        //      (those that can be reached in one move from the dequeued search node).
+        SearchNode curr = queue.delMin();
+        SearchNode twinCurr = twinQueue.delMin();
+        while (!twinCurr.board.isGoal() && !curr.board.isGoal()) {
+            for (Board neighbor : curr.board.neighbors()) {
+                // Critical optimization
+                if (curr.prevNode != null && neighbor.equals(curr.prevNode.board)) {
+                    continue;
+                }
+                SearchNode neighborNode = new SearchNode(neighbor, curr);
+                queue.insert(neighborNode);
+            }
+            for (Board neighbor : twinCurr.board.neighbors()) {
+                SearchNode neighborNode = new SearchNode(neighbor, twinCurr);
+                twinQueue.insert(neighborNode);
+            }
+
+            curr = queue.delMin();
+            twinCurr = twinQueue.delMin();
+        }
+        if (curr.board.isGoal()) {
+            this.solvable = true;
+            this.LastNode = curr;
+        }
+    }
+
+
+    // is the initial board solvable?
+    public boolean isSolvable() {
+        return this.solvable;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+        if (!this.solvable) return -1;
+        return this.LastNode.move;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+        if (!this.solvable) return null;
+
+        Stack<Board> solutionBoards = new Stack<>();
+        SearchNode curr = this.LastNode;
+        while (curr != null) {
+            solutionBoards.push(curr.board);
+            curr = curr.prevNode;
+        }
+        return solutionBoards;
+    }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+}
